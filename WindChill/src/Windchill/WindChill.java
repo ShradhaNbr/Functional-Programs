15b4f892b2e4058c2a7c7cf6655ff891ed83f3b6
ShradhaNbr <sradha.nambiar@gmail.com>
2021-07-08 18:57:26 +0530

    Added code to calculate Wind_Chill

--- WindChill/src/Windchill/WindChill.java
+++ WindChill/src/Windchill/WindChill.java
@@ -1,5 +1,25 @@
 package Windchill;
 
-public class WindChill {
+import java.util.Scanner;
 
+public class WindChill {
+	public static void main(String[] args) {
+		double v, t;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter temperature in Fahrenheit");
+		t = sc.nextDouble();
+		System.out.println("Enter wind speed");
+		v = sc.nextDouble();
+		windchill(t, v);
+		sc.close();
+	}
+	public static void windchill(double t, double v) {
+		if (t>50 || v>120 || v<3)
+			System.out.println("Invalid Input");
+		else {
+		   double w = 35.74 + 0.6215 * t + ((0.4275 * t - 35.75) * Math.pow(v, 0.16)); 
+		System.out.println("Windchill for temperature" + t + "and windchill" + v + "is" + w );
+		}
+		
+	}
 }
